b67bc1855671f377b55c2cffc1fa21052e0a8672
danqing <49900493+changfeng0627@users.noreply.github.com>
2026-08-12 15:44:06 +0800

    copy: app/src/main/kotlin/fuck/andes/data/model/Settings.kt

---
+++ app/src/main/kotlin/fuck/andes/data/model/Settings.kt
@@ -0,0 +1,10 @@
+package fuck.andes.data.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Settings(
+    val selectedProviderId: String? = null,
+    val selectedModelId: String? = null,
+    val memoryEnabled: Boolean = true,
+)
