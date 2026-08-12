a797f4fd1edba20f4c6b28372a335408f637fb60
danqing <49900493+changfeng0627@users.noreply.github.com>
2026-08-12 15:41:55 +0800

    copy: app/src/main/kotlin/fuck/andes/agent/voice/EtaVoiceInteractionSessionService.kt

---
+++ app/src/main/kotlin/fuck/andes/agent/voice/EtaVoiceInteractionSessionService.kt
@@ -0,0 +1,10 @@
+package fuck.andes.agent.voice
+
+import android.os.Bundle
+import android.service.voice.VoiceInteractionSession
+import android.service.voice.VoiceInteractionSessionService
+
+class EtaVoiceInteractionSessionService : VoiceInteractionSessionService() {
+    override fun onNewSession(args: Bundle): VoiceInteractionSession =
+        EtaVoiceInteractionSession(this)
+}
